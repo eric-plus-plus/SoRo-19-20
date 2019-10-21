01cf430248dee9fcf4d98f60bc4191af7a68da22
RoverTeam <soro@Jacob2.0>
2019-10-21 18:04:35 -0500

    added things to be able to test it better. There is something really wrong with distance but the angle seems okay

--- Autonomous/ARTester.cpp
+++ Autonomous/ARTester.cpp
@@ -1,6 +1,19 @@
 #include <ARTracker.h>
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
 
 int main()
 {
     ARTracker* tracker = new ARTracker("/dev/video0");
+    cv::namedWindow("win");
+    while(true)
+    {
+        cout << tracker->findAR(0) << endl;
+        cout << "distance: " << tracker->distanceToAR << endl;
+        cout << "angle: " << tracker->angleToAR << endl;
+        cv::imshow("win", tracker->frame);
+        cv::waitKey(100);
+    }
 }
