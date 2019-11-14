edb1682075361de18d60833a0b4b239ff47d8846
Eric Rackelin <33244667+eric-plus-plus@users.noreply.github.com>
2019-11-13 18:35:56 -0600

    Wheel controller bug fixes
    It works now and also has the right pins

--- Arduino/wheel_controller/wheel_controller.ino
+++ Arduino/wheel_controller/wheel_controller.ino
@@ -2,17 +2,20 @@
 #include <IPAddress.h>
 #include <Servo.h>
 
+//Set equal to 1 for serial debugging
+#define DEBUG_MODE 0
+
 /*
      Pin wiring diagram
-     
+
      [],pin  front   [],pin
      0,2     0-|-0    3,7
                |
      1,3     0-|-0   -4,6
                |
     -2,4     0-|-0   -5,5
-     "-" indicaes wheel is polarity needs to be reversed
-     
+     "-" indicaes wheel's polarity needs to be reversed
+
      gimbal pan: A2
      gimbal tilt: A1
      brake disk: A3
@@ -23,7 +26,7 @@
 */
 // Note: analogWrite() can not be used on pins 9 or 10 because of the servo library.
 // Servos can still be used on those pins though
-#define WHEEL_FL 2
+#define WHEEL_FL A4
 #define WHEEL_ML 3
 #define WHEEL_RL 4
 
@@ -38,18 +41,17 @@
 #define JETSON_POWER A0
 
 // ethernet interface ip address (static ip)
-static byte myip[] = { 10,0,0,101 };
+static byte myip[] = { 10, 0, 0, 101 };
 // gateway ip address
-static byte gwip[] = { 10,0,0,1 };
+static byte gwip[] = { 10, 0, 0, 1 };
 // ethernet mac address - must be unique on your network
-static byte mymac[] = { 0x70,0x69,0xFF,0xFF,0x30,0x31 };
+static byte mymac[] = { 0x70, 0x69, 0xFF, 0xFF, 0x30, 0x31 };
 // tcp/ip send and receive buffer
 byte Ethernet::buffer[500];
 
 // wheel_controller stuff
 const char DEVICE_ID = 0;
 Servo wheel[6], gimbal_pan, gimbal_tilt, disk;
-
 char myHash = 0;
 char serialHash = 0;
 char pan = 0;
@@ -63,73 +65,84 @@ unsigned long timeOut = 0;
 
 
 //callback that prints received packets to the serial port
-void udpSerialPrint(uint16_t dest_port, uint8_t src_ip[IP_LEN], uint16_t src_port, const char *data, uint16_t len){
-  IPAddress src(src_ip[0],src_ip[1],src_ip[2],src_ip[3]);
-  timeOut = millis();
-  
+void udpSerialPrint(uint16_t dest_port, uint8_t src_ip[IP_LEN], uint16_t src_port, const char *data, uint16_t len) {
+  IPAddress src(src_ip[0], src_ip[1], src_ip[2], src_ip[3]);
+
   // serial transmission blueprint:
   // [start transmission = -127 or 255] [device id] [overdrive] [left wheels]...
   // ...[right wheels] [gimble tilt] [gimble pan] [hash]
   // hash = (sum of data bytes--no start or id) / (num of bytes)
   // all message bytes will be within the range [-90,90]
-  
-  // serial debug output
-  for(int i = 0; i < len; i++)
+
+#if DEBUG_MODE == 1
+  for (int i = 0; i < len; i++)
   {
     Serial.print(int(data[i]));
     Serial.print(", ");
   }
   Serial.print("\n");
-  
+#endif
 
-  if(len != 8)
+  if (len != 8)
   {
+#if DEBUG_MODE == 1
     Serial.println("Message is wrong length!");
-    return;
-  }
-  
-  if(data[0] != -127)
-  {
-    Serial.println("Message does not start with -127!");
+#endif
+
     return;
   }
 
-  if(data[1] != 0)
+  if (data[0] != -127)
   {
+#if DEBUG_MODE == 1
+    Serial.println("Message does not start with -127!");
+#endif
+
+    return;
+  }
+
+  if (data[1] != 0)
+  {
+#if DEBUG_MODE == 1
     Serial.println("Wrong device id!");
+#endif
+
     return;
   }
 
   modifiers = data[2];
-  
+
   leftWheels = data[3];
-  
+
   rightWheels = data[4];
 
   tiltByte = data[5];
   tilt += tiltByte;
-  if(tilt <40 || tilt >= 127)
-      tilt -= tiltByte;
+  if (tilt < 40 || tilt >= 127)
+    tilt -= tiltByte;
 
   pan = data[6];
 
   serialHash = data[7];
-  myHash = (pan + tiltByte + leftWheels + rightWheels + modifiers)/5;
-  if(myHash == serialHash)
+  myHash = (pan + tiltByte + leftWheels + rightWheels + modifiers) / 5;
+  if (myHash == serialHash)
   {
     //sprintf(serResp, "%d\t%d\t%d\t%d\t%d\t%d", overdrive, leftWheels, rightWheels, tilt, pan, serialHash);
     //Serial.println(serResp);
     updateServos();
+    timeOut = millis();
   }
 }
 
-void setup(){
+void setup() {
+#if DEBUG_MODE == 1
   Serial.begin(9600);
-
-  // Change 'SS' to your Slave Select pin, if you aren't using the default pin
-  // Our ethernet card uses pin 10. SS is 8 by default. --Eric
   if (ether.begin(sizeof Ethernet::buffer, mymac, 10) == 0)
     Serial.println(F("Failed to access Ethernet controller"));
+#else
+  ether.begin(sizeof Ethernet::buffer, mymac, 10);
+#endif
+
   ether.staticSetup(myip, gwip);
 
   ether.printIp("IP:  ", ether.myip);
@@ -149,7 +162,7 @@ void setup(){
   gimbal_pan.attach(SERVO_PAN);
   gimbal_tilt.attach(SERVO_TILT);
   disk.attach(SERVO_BRAKE);
-  
+
   wheel[0].write(90);
   wheel[1].write(90);
   wheel[2].write(90);
@@ -161,17 +174,19 @@ void setup(){
   disk.write(90);
 
   pinMode(JETSON_POWER, OUTPUT);
+
+  delay(5000);
   digitalWrite(JETSON_POWER, HIGH);
-  delay(500);
+  delay(2000);
   digitalWrite(JETSON_POWER, LOW);
 }
 
-void loop(){
+void loop() {
   // this must be called for ethercard functions to work.
   ether.packetLoop(ether.packetReceive());
 
   // stop all motors after 1 second of no messages
-  if( millis() - timeOut >= 1000)
+  if ( millis() - timeOut >= 1000)
   {
     timeOut = millis();
     wheel[0].write(90);
@@ -183,11 +198,13 @@ void loop(){
     gimbal_tilt.write(90);
     disk.write(90);
 
+#if DEBUG_MODE == 1
     Serial.println("Stopped motors");
+#endif
   }
 }
 
-void updateServos(){
+void updateServos() {
   int speeds[6];
   speeds[0] = 90 + (leftWheels * 0.9);
   speeds[1] = 90 + (leftWheels * 0.9);
@@ -195,43 +212,43 @@ void updateServos(){
   speeds[3] = 90 + (rightWheels * 0.9);
   speeds[4] = 90 - (rightWheels * 0.9);
   speeds[5] = 90 - (rightWheels * 0.9);
-   if((modifiers & 1)){
-      disk.write(180);
-   }else{
-      disk.write(90);
-   }
-   if(modifiers & 8){
-      gimbal_pan.write(90);
-      gimbal_tilt.write(90);
-   }else{
-      gimbal_pan.write(93 + pan);
-      gimbal_tilt.write(tilt);
-   }
-   //
-   if((modifiers & 2) && ( modifiers & 4)){
-      wheel[0].write(speeds[0]);
-      wheel[3].write(speeds[3]);
-      wheel[2].write(speeds[2]);
-      wheel[5].write(speeds[5]);
-   }
-   // 
-   if(modifiers & 2){
-      wheel[0].write(speeds[0]);
-      wheel[3].write(speeds[3]);
-  return;
-   }
-   // just the front wheels
-   if(modifiers & 4){
-       wheel[2].write(speeds[2]);
-       wheel[5].write(speeds[5]);
-       return;
-   } else {
-      wheel[0].write(speeds[0]);// * 1/2));
-      wheel[1].write(speeds[1]);
-      wheel[2].write(speeds[2]);
-      wheel[3].write(speeds[3]);
-      wheel[4].write(speeds[4]);
-      wheel[5].write(speeds[5]);  
-   }
-   
+  if ((modifiers & 1)) {
+    disk.write(180);
+  } else {
+    disk.write(90);
+  }
+  if (modifiers & 8) {
+    gimbal_pan.write(90);
+    gimbal_tilt.write(90);
+  } else {
+    gimbal_pan.write(93 + pan);
+    gimbal_tilt.write(tilt);
+  }
+  //
+  if ((modifiers & 2) && ( modifiers & 4)) {
+    wheel[0].write(speeds[0]);
+    wheel[3].write(speeds[3]);
+    wheel[2].write(90-(speeds[2]-90));
+    wheel[5].write(90-(speeds[5]-90));
+  }
+  //
+  if (modifiers & 2) {
+    wheel[0].write(speeds[0]);
+    wheel[3].write(speeds[3]);
+    return;
+  }
+  // just the front wheels
+  if (modifiers & 4) {
+    wheel[2].write(speeds[2]);
+    wheel[5].write(speeds[5]);
+    return;
+  } else {
+    wheel[0].write(speeds[0]);// * 1/2));
+    wheel[1].write(speeds[1]);
+    wheel[2].write(speeds[2]);
+    wheel[3].write(speeds[3]);
+    wheel[4].write(speeds[4]);
+    wheel[5].write(speeds[5]);
+  }
+
 }
