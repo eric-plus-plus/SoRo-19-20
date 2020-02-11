2370f5e98dc5d153bda77327eac50a88f483c59d
RezaTorbati <kofcrotmgiv@gmail.com>
2020-02-11 22:51:31 +0000

    Added code so the rover will drive to a second tag after getting to the first

--- Autonomous/main.cpp
+++ Autonomous/main.cpp
@@ -22,7 +22,24 @@ int main(int argc, char* argv[])
     }
     rover.driveAlongCoordinates(locations, 5);
     rover.trackARTag(5);
+   
+    locations.clear(); 
+    while(true)
+    {
+        std::vector<double> point;
+        std::cout<<"Enter lat and lon: " << std::endl;
+        std::cin >> lat;
+        std::cin >> lon;
+        if(lat == -1 && lon == -1)
+            break;
+        point.push_back(lat);
+        point.push_back(lon);
+        locations.push_back(point);
+    }
+    rover.driveAlongCoordinates(locations, 0);
+    rover.trackARTag(0);
 
+    std::cout << "Finished everything!" << std::endl;
     std::cout << "\ndon't worry about the next error" << std::endl;
     rover.locationInst.stopGPS();
     return 0;
