331645f8cd148239a640d05533fbf3af6d800d4e
RoverTeam <soro@Jacob2.0>
2020-02-23 14:55:07 -0600

    added 2 post functionality

--- Autonomous/libs/DriveMode.cpp
+++ Autonomous/libs/DriveMode.cpp
@@ -116,6 +116,56 @@ bool DriveMode::driveAlongCoordinates(std::vector<std::vector<double>> locations
     return false; //got to gps location without finding the wanted ar tag
 }
 
+bool DriveMode::driveAlongCoordinates(std::vector<std::vector<double>> locations, int id1, int id2) //used for legs 4-7
+{    
+    locationInst.startGPSThread();
+
+    std::cout<<"Waiting for GPS connection..." << std::endl;
+    while(locationInst.allZero); //waits for the GPS to pick something up before starting
+    std::cout << "Connected to GPS" << std::endl; 
+     
+    //Drives for 4 seconds to hopefully get a good angle from the gps
+    leftWheelSpeed = speed;
+    rightWheelSpeed = speed;
+    printSpeeds();
+    cv::waitKey(4000);
+    
+    float bearingTo;
+    std::vector<double> wheelSpeeds;
+    for(int i = 0; i < locations.size(); ++i)
+    {
+        time = 0;
+        errorAccumulation = 0;
+        while(locationInst.distanceTo(locations[i][0], locations[i][1]) > 0.001) //.001km
+        {
+            bearingTo = locationInst.bearingTo(locations[i][0], locations[i][1]);
+            wheelSpeeds = getWheelSpeeds(bearingTo, speed);
+            leftWheelSpeed = wheelSpeeds[1];
+            rightWheelSpeed = wheelSpeeds[0];
+            printSpeeds();            
+
+            cv::waitKey(100); //waits for 100ms
+            time += 100; //updates time
+            
+            if(tracker.findARs(id1, id2))
+            {
+                locationInst.stopGPSThread();
+                std::cout << "Found tags!!!" << std::endl;
+                return true;
+            }
+        }
+        if(tracker.findARs(id1, id2))
+        {
+            locationInst.stopGPSThread();
+            std::cout << "Found tags!!!" << std::endl;
+            return true;
+        }
+    }
+    locationInst.stopGPSThread();
+    std::cout << "Made it to the gps location without seeing both tags..." << std::endl;
+    return false; //got to gps location without finding the wanted ar tag
+}
+
 bool DriveMode::trackARTag(int id) //used for legs 1-3
 {
     std::string str;
@@ -174,6 +224,94 @@ bool DriveMode::trackARTag(int id) //used for legs 1-3
         if(tracker.trackAR(id) || timesNotFound < 10)
         {
             if(tracker.trackAR(id))
+            {
+                wheelSpeeds = getWheelSpeeds(tracker.angleToAR, speed);
+                timesNotFound = 0;
+            }
+            else
+            {
+                std::cout << "Didn't find it " << timesNotFound + 1 << " times" << std::endl;
+                timesNotFound++;
+            }
+            std::cout << tracker.angleToAR << " " << tracker.distanceToAR << std::endl;
+            leftWheelSpeed = wheelSpeeds[1];
+            rightWheelSpeed = wheelSpeeds[0];
+            printSpeeds();
+        }
+        else
+        {
+            //Stops the rover
+            leftWheelSpeed = 0;
+            rightWheelSpeed = 0;
+            std::cout << "Tag not found" << std::endl;
+            return false; //TODO: do something about this
+        }
+        cv::waitKey(100); //waits for 100ms    
+        time += 100;
+    }
+    leftWheelSpeed = 0;
+    rightWheelSpeed = 0;
+    return true;
+}
+
+bool DriveMode::trackARTags(int id1, int id2) //used for legs 4-7
+{
+    std::string str;
+    std::vector<double> wheelSpeeds;
+    int timesNotFound = -1;
+    int stopDistance = 250;  //drives until the distance to the tag is less than stopDistance in cm. NOTE: rover only needs to be within 300cm to score.
+    
+    tracker.trackARs(id1, id2); //gets an intial angle from the main camera
+    
+    //turns to face the artag directly before driving to it. May want to convert to PID although this also shouldn't have to be super accurate.
+    while(tracker.angleToAR > 30 || tracker.angleToAR < -25 || tracker.angleToAR == 0) //its 0 if it doesn't see it, camera is closer to the left which is why one is 10 and the other is -5
+    {
+        if(tracker.trackARs(id1, id2))
+        {            
+            if(tracker.angleToAR > 30)
+            {
+                std::cout << "turning right" << std::endl;
+                leftWheelSpeed = 45;
+                rightWheelSpeed = -45;
+            }
+            else
+            {
+                std::cout << "turning left" << std::endl;
+                leftWheelSpeed = -45;
+                rightWheelSpeed = 45;
+            }
+            std::cout << tracker.angleToAR << " " << tracker.distanceToAR << std::endl;
+            timesNotFound = 0;
+        }
+        else if(timesNotFound == -1)// hasn't seen anything yet so turns to the left until it sees it
+        {
+            leftWheelSpeed = -45;
+            rightWheelSpeed = 45;
+            std::cout << "Haven't seen it so turning left" << std::endl;
+        }
+        else if(timesNotFound < 10)
+        {
+            timesNotFound++;
+            std::cout << "Didn't find it " << timesNotFound << " times" << std::endl;
+        }
+        else
+        {
+            leftWheelSpeed = 0;
+            rightWheelSpeed = 0;
+            std::cout << "we lost it..." << std::endl;
+            return false; //TODO: do something about this
+        }
+        cv::waitKey(100);
+    }
+    
+    time = 0;
+    errorAccumulation = 0; 
+    std::cout << "\nWe are locked on and ready to track!\n" << std::endl;
+    while(tracker.distanceToAR > stopDistance || tracker.distanceToAR == -1) //distance = -1 if the camera cannot find a tag
+    {
+        if(tracker.trackARs(id1, id2) || timesNotFound < 10)
+        {
+            if(tracker.trackARs(id1, id2))
             {
                 wheelSpeeds = getWheelSpeeds(tracker.angleToAR, speed);
                 timesNotFound = 0;
