2ae3fd21ca882a2b9d07d7a3343212e9b6b6fe7a
bentonsmith8 <bentonsmith@ou.edu>
2021-03-01 19:09:27 -0600

    pulls MJPG from cameras, added comments to code

--- Mission Control/video_streamer/client.py
+++ Mission Control/video_streamer/client.py
@@ -1,147 +1,137 @@
 from flask import Response
 from flask import Flask
 from flask import render_template
-import threading
 import argparse
 import datetime
 from videostream import VideoStream
-from threading import Thread
 import imutils
-import time
 import cv2
 
 
 # initialize the output frame and a lock used to ensure thread-safe
 # exchanges of the output frames (useful for multiple browsers/tabs
 # are viewing tthe stream)
+# benton note: maybe this might be important to put back in at some point
+# but it seems to work fine without it so i'll leave it as a comment for now
 # lock = threading.Lock()
 
 # initialize a flask object
 app = Flask(__name__)
 
+# initialize the video stream with the desired fps
+def startStream(src, fps):
+		print('stream started')
+		return VideoStream(src, fps).start()
 
-# initialize the video stream and allow the camera sensor to
-# warmup
-def startStream(src):
-	print('stream started')
-	return VideoStream(src).start()
-
+# define the root url to be index.html
+# in other words, when the user accesses http://[ip]:[port]/
+# they will see index.html
 @app.route("/")
 def index():
-	# return the rendered template
-	return render_template("index.html")
+		# return the rendered template
+		return render_template("index.html")
 
-def get_frame(vs):
-	
-	while vs is not None:
-		frame = vs.read()
-		# frame = imutils.resize(frame, width=600)
-
-		# print("frame has been read")
-
-		# grab the current timestamp and draw it on the frame
-		# may not be useful and can be disabled
+# grab the current timestamp and draw it on the frame
+def timestampframe(frame):
 		timestamp = datetime.datetime.now()
 		cv2.putText(frame, timestamp.strftime(
-			"%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+			  "%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
+			  cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
 
-		# acquire the lock, set the output frame, and release the
-		# lock
-		# with lock:
-		# print('frame returned')
 		return frame.copy()
-			
+						
 def generate():
-	# grab global references to the output frame and lock variables
-	# global vs, vs2
-	global vslist
-	framelist = []
+		# grab global references to the list of video streams
+		# and create list for frames to be stored in
+		global vslist
+		framelist = []
 
-	# loop over frames from the output stream
-	while True:
+		# loop over frames from the output stream
+		while True:
+				# read a frame from each vs in vslist and add it
+				# to framelist
+				for vs in vslist:
+						framelist.append(vs.read())
 
-		for vs in vslist:
-			framelist.append(vs.read())
+				# resize each frame to 600
+				for frame in framelist:
+						frame = imutils.resize(frame, width=600)
+					
+				# wait until the lock is acquired
+				# with lock:
 
-		# wait until the lock is acquired
-		# with lock:
-			# check if the output frame is available, otherwise skip
-			# the iteration of the loop
-		
-		mergedim = cv2.hconcat(framelist)
-		framelist.clear()
-			# encode the frame in JPEG format
-		encoding_parameters = [int(cv2.IMWRITE_JPEG_QUALITY), 50]
-		(flag, encodedImage) = cv2.imencode(".jpg", mergedim, encoding_parameters)
+				# merge each of the three frames into one image
+				mergedim = cv2.hconcat(framelist)
 
-			# ensure the frame was successfully encoded
-		if not flag:
-			continue
+				# add the timestamp to the merged image
+				timestampframe(mergedim)
 
-		# send = Falsehreading.Thread(target=get_frame, args=(vs))
-		# t.daemon = True
-		# t.star
-		# yield the output frame in the byte format
-		yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
-			bytearray(encodedImage) + b'\r\n')
-			# print('image sent')
+				# clear the framelist for the next iteration
+				framelist.clear()
 
+				# encode the frame in JPEG format
+				encoding_parameters = [int(cv2.IMWRITE_JPEG_QUALITY), 50]
+				(flag, encodedImage) = cv2.imencode(".jpg", mergedim, encoding_parameters)
+
+				# ensure the frame was successfully encoded
+				if not flag:
+						continue
+
+				# yield the output frame in the byte format
+				yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
+						bytearray(encodedImage) + b'\r\n')
+
+# return the generated image to the 'video_feed' element of index.html
 @app.route("/video_feed")
 def video_feed():
-	# return the response generated along with the specific media
-	# type (mime type)
-	return Response(generate(),
-		mimetype = "multipart/x-mixed-replace; boundary=frame")
+		# return the response generated along with the specific media
+		# type (mime type)
+		return Response(generate(),
+				mimetype = "multipart/x-mixed-replace; boundary=frame")
 
 # check to see if this is the main thread of execution
 if __name__ == '__main__':
 
-	# construct the argument parser and parse command line arguments
-	ap = argparse.ArgumentParser()
-	ap.add_argument("-i", "--ip", type=str, required=True,
-		help="ip address of the device")
-	ap.add_argument("-o", "--port", type=int, required=True,
-		help="ephemeral port number of the server (1024 to 65535)")
-	ap.add_argument("-s1", "--source1", type=int, default=0,
-		help="index of camera to use")
-	ap.add_argument("-s2", "--source2", type=int, default=-1,
-		help="index of camera to use")
-	ap.add_argument("-s3", "--source3", type=int, default=-1,
-		help="index of camera to use")
+		# construct the argument parser and parse command line arguments
+		ap = argparse.ArgumentParser()
+		ap.add_argument("-i", "--ip", type=str, required=True,
+				help="ip address of the device")
+		ap.add_argument("-o", "--port", type=int, required=True,
+				help="port number of the server (1024 to 65535)")
+		ap.add_argument("-s1", "--source1", type=int, default=0,
+				help="index of first camera to use")
+		ap.add_argument("-s2", "--source2", type=int, default=-1,
+				help="index of second camera to use")
+		ap.add_argument("-s3", "--source3", type=int, default=-1,
+				help="index of third camera to use")
+		ap.add_argument("-f", "--fps", type=int, default=15,
+				help="fps of stream")
 
-	args = vars(ap.parse_args())
-	# start a thread that will grab frames from camera
-	vs = startStream(args["source1"])
-	vslist = [vs]
-	if args["source2"] != -1:
-		vs2 = startStream(args["source2"])
-		vslist.append(vs2)
-	if args["source3"] != -1:
-		vs3 = startStream(args["source3"])
-		vslist.append(vs3)
+		# parse arguments
+		args = vars(ap.parse_args())
 
-	# vs.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M','J','P','G'))
+		# start a thread that will grab frames from camera
+		vs = startStream(args["source1"], args["fps"])
 
-	print(vs.get(cv2.CAP_PROP_FOURCC))
-	print(vs.get(cv2.CAP_PROP_FPS))
+		# create a list of video streams to reference in generate()
+		vslist = [vs]
 
-	print(vs.stream.get(cv2.CAP_PROP_FRAME_HEIGHT))
-	print(vs.stream.get(cv2.CAP_PROP_FRAME_WIDTH))
+		# if a second source has been defined, start a new video stream 
+		if args["source2"] != -1:
+				vs2 = startStream(args["source2"], args["fps"])
+				vslist.append(vs2)
 
-	for vs in vslist:
-		pass
-		# vs.set(cv2.CAP_PROP_FPS, 5)
-		# t = threading.Thread(target=get_frame, args=(vs))
-		# t.daemon = True
-		# t.start()
+		# if a third source has been defined, start a new video stream 
+		if args["source3"] != -1:
+				vs3 = startStream(args["source3"], args["fps"])
+				vslist.append(vs3)
 
-	# start the flask app
-	app.run(host=args["ip"], port=args["port"], debug=True,
-		threaded=True, use_reloader=False)
+		# start the flask app
+		app.run(host=args["ip"], port=args["port"], debug=True,
+				threaded=True, use_reloader=False)
 
 # release the video stream pointer
 for vs in vslist:
-	vs.stop()
+		vs.stop()
 
-    
+	
--- Mission Control/video_streamer/videostream.py
+++ Mission Control/video_streamer/videostream.py
@@ -2,35 +2,52 @@ from threading import Thread
 import cv2
 
 class VideoStream:
-    def __init__(self, src=0, name="VideoStream"):
+    # initialize self with defined source and fps
+    def __init__(self, src=0, fps=15,  name="VideoStream"):
         self.stream = cv2.VideoCapture(src)
+
+        # set the capture codec to MJPG
+        self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M','J','P','G'))
+
+        # set fps to argument
+        self.stream.set(cv2.CAP_PROP_FPS, fps)
+
+        # read the first frame in
         (self.grabbed, self.frame) = self.stream.read()
 
+        # set the name of the object
         self.name = name
 
+        # define a variable to indicate whether the thread should be stopped
         self.stopped = False
     
+    # create a new thread to continuously call update() and read frames
     def start(self):
         t = Thread(target=self.update, name=self.name, args=())
         t.daemon = True
         t.start()
-        print('thread started')
+        # print('thread started')
         return self
     
+    # read a frame from the camera unless stopped is true
     def update(self):
         while True:
             if self.stopped:
                 return
             (self.grabbed, self.frame) = self.stream.read()
     
+    # return the most recent frame grabbed from the camera
     def read(self):
         return self.frame
 
+    # set stopped to True which will halt the thread
     def stop(self):
         self.stopped = True
 
+    # sets the property of the VideoCapture object
     def set(self, property, value):
         self.stream.set(property, value)
     
+    # returns the property of the VideoCapture object
     def get(self, property):
         return self.stream.get(property)
