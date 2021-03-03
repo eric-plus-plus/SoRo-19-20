9195d9d408a918558bbf3ef77e88c12411f49331
bentonsmith8 <bentonsmith8@users.noreply.github.com>
2021-03-02 18:53:52 -0600

    added ability to open up to 6 cameras and turn them on and off at runtime

--- Mission Control/video_streamer/client.py
+++ Mission Control/video_streamer/client.py
@@ -1,12 +1,9 @@
-from flask import Response
-from flask import Flask
-from flask import render_template
+from flask import Response, Flask, render_template, request
 import argparse
 import datetime
 from videostream import VideoStream
 import imutils
 import cv2
-
 
 # initialize the output frame and a lock used to ensure thread-safe
 # exchanges of the output frames (useful for multiple browsers/tabs
@@ -19,17 +16,26 @@ import cv2
 app = Flask(__name__)
 
 # initialize the video stream with the desired fps
-def startStream(src, fps):
+def startstream(src, fps, name):
 		print('stream started')
-		return VideoStream(src, fps).start()
+		return VideoStream(src, fps, name).start()
 
 # define the root url to be index.html
 # in other words, when the user accesses http://[ip]:[port]/
 # they will see index.html
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def index():
+		# add button handlers depending on number of cameras
+		camerasconnected = vslist.__len__()
+		for i in range(0, camerasconnected):
+			if 'stopvs{}'.format(i+1) in request.form:
+				vslist[i].stop()
+			if 'startvs{}'.format(i+1) in request.form:
+				vslist[i].start()
 		# return the rendered template
-		return render_template("index.html")
+		return render_template("index.html", 
+				camerasconnected=camerasconnected,
+				activestreams=activestreams)
 
 # grab the current timestamp and draw it on the frame
 def timestampframe(frame):
@@ -37,22 +43,29 @@ def timestampframe(frame):
 		cv2.putText(frame, timestamp.strftime(
 			  "%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
 			  cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-
 		return frame.copy()
-						
+
 def generate():
 		# grab global references to the list of video streams
 		# and create list for frames to be stored in
-		global vslist
-		framelist = []
-
+		global vslist, activestreams, framelist
 		# loop over frames from the output stream
 		while True:
 				# read a frame from each vs in vslist and add it
-				# to framelist
+				# to framelist if it is readable
+				activestreams = 0
 				for vs in vslist:
-						framelist.append(vs.read())
+						if vs.isreadable():
+							activestreams+=1
+							framelist.append(vs.read())
+						# else:
+						# 	t = Thread(target=vs.relaunch, args=())
+						# 	t.daemon = True
+						# 	t.start()
 
+				if framelist.__len__() == 0:
+					continue
+				
 				# resize each frame to 600
 				for frame in framelist:
 						frame = imutils.resize(frame, width=600)
@@ -71,7 +84,7 @@ def generate():
 
 				# encode the frame in JPEG format
 				encoding_parameters = [int(cv2.IMWRITE_JPEG_QUALITY), 50]
-				(flag, encodedImage) = cv2.imencode(".jpg", mergedim, encoding_parameters)
+				(flag, encodedimage) = cv2.imencode(".jpg", mergedim, encoding_parameters)
 
 				# ensure the frame was successfully encoded
 				if not flag:
@@ -79,7 +92,7 @@ def generate():
 
 				# yield the output frame in the byte format
 				yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
-						bytearray(encodedImage) + b'\r\n')
+						bytearray(encodedimage) + b'\r\n')
 
 # return the generated image to the 'video_feed' element of index.html
 @app.route("/video_feed")
@@ -91,40 +104,62 @@ def video_feed():
 
 # check to see if this is the main thread of execution
 if __name__ == '__main__':
-
 		# construct the argument parser and parse command line arguments
 		ap = argparse.ArgumentParser()
 		ap.add_argument("-i", "--ip", type=str, required=True,
 				help="ip address of the device")
 		ap.add_argument("-o", "--port", type=int, required=True,
 				help="port number of the server (1024 to 65535)")
+
+		# allow up to 6 video streams to be created
 		ap.add_argument("-s1", "--source1", type=int, default=0,
 				help="index of first camera to use")
 		ap.add_argument("-s2", "--source2", type=int, default=-1,
 				help="index of second camera to use")
 		ap.add_argument("-s3", "--source3", type=int, default=-1,
 				help="index of third camera to use")
+		ap.add_argument("-s4", "--source4", type=int, default=-1,
+				help="index of fourth camera to use")
+		ap.add_argument("-s5", "--source5", type=int, default=-1,
+				help="index of fifth camera to use")
+		ap.add_argument("-s6", "--source6", type=int, default=-1,
+				help="index of sixth camera to use")
+
+		# choose fps
 		ap.add_argument("-f", "--fps", type=int, default=15,
 				help="fps of stream")
 
 		# parse arguments
 		args = vars(ap.parse_args())
-
+		fps = args["fps"]
 		# start a thread that will grab frames from camera
-		vs = startStream(args["source1"], args["fps"])
+		vs = startstream(args["source1"], fps, 'vs1')
 
 		# create a list of video streams to reference in generate()
 		vslist = [vs]
+		activestreams = 1
+		framelist = []
 
 		# if a second source has been defined, start a new video stream 
 		if args["source2"] != -1:
-				vs2 = startStream(args["source2"], args["fps"])
+				vs2 = VideoStream(args["source2"], fps, 'vs2')
 				vslist.append(vs2)
 
 		# if a third source has been defined, start a new video stream 
 		if args["source3"] != -1:
-				vs3 = startStream(args["source3"], args["fps"])
+				vs3 = VideoStream(args["source3"], fps, 'vs3')
 				vslist.append(vs3)
+
+		# and so on...
+		if args["source4"] != -1:
+				vs4 = VideoStream(args["source4"], fps, 'vs4')
+				vslist.append(vs4)
+		if args["source5"] != -1:
+				vs5 = VideoStream(args["source5"], fps, 'vs5')
+				vslist.append(vs5)
+		if args["source6"] != -1:
+				vs6 = VideoStream(args["source6"], fps, 'vs6')
+				vslist.append(vs6)
 
 		# start the flask app
 		app.run(host=args["ip"], port=args["port"], debug=True,
--- Mission Control/video_streamer/videostream.py
+++ Mission Control/video_streamer/videostream.py
@@ -5,15 +5,16 @@ class VideoStream:
     # initialize self with defined source and fps
     def __init__(self, src=0, fps=15,  name="VideoStream"):
         self.stream = cv2.VideoCapture(src)
-
+        self.src = src
+        self.fps = fps
         # set the capture codec to MJPG
         self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M','J','P','G'))
 
         # set fps to argument
         self.stream.set(cv2.CAP_PROP_FPS, fps)
 
-        # read the first frame in
-        (self.grabbed, self.frame) = self.stream.read()
+        # set grabbed to false
+        self.grabbed = False
 
         # set the name of the object
         self.name = name
@@ -23,6 +24,8 @@ class VideoStream:
     
     # create a new thread to continuously call update() and read frames
     def start(self):
+        print('starting ' + self.name)
+        self.stopped = False
         t = Thread(target=self.update, name=self.name, args=())
         t.daemon = True
         t.start()
@@ -33,6 +36,7 @@ class VideoStream:
     def update(self):
         while True:
             if self.stopped:
+                self.grabbed = False
                 return
             (self.grabbed, self.frame) = self.stream.read()
     
@@ -42,6 +46,7 @@ class VideoStream:
 
     # set stopped to True which will halt the thread
     def stop(self):
+        print('stopping ' + self.name)
         self.stopped = True
 
     # sets the property of the VideoCapture object
@@ -51,3 +56,23 @@ class VideoStream:
     # returns the property of the VideoCapture object
     def get(self, property):
         return self.stream.get(property)
+
+    # returns whether a frame can be read
+    def isreadable(self):
+        return self.grabbed
+
+    # returns index of camera
+    def getsrc(self):
+        return self.src
+    
+    # releases video capture object, doesn't work lol
+    def release(self):
+        self.stop()
+        self.stream.release()
+
+    # also doesn't work, just don't touch this for now
+    def relaunch(self):
+        print('relaunch has been called')
+        self.release()
+        self.__init__(self.src, self.fps)
+
