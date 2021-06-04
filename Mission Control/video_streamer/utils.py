2e204b39c31ed75dfa13fdc6f9b2ce8837569689
bentonsmith8 <bentonsmith@ou.edu>
2021-06-04 15:43:37 -0500

    record button fix and some css

--- Mission Control/video_streamer/utils.py
+++ Mission Control/video_streamer/utils.py
@@ -2,14 +2,18 @@ import cv2
 import datetime
 
 # find the number of streams actively providing frames
+
+
 def findactivestreams(vslist):
 	activestreams = 0
 	for vs in vslist:
 		if vs.stopped == False:
 			activestreams += 1
-	return activestreams	
+	return activestreams
 
 # find number of video streams actively recording
+
+
 def findrecording(vslist):
 	recording = []
 	for vs in vslist:
@@ -17,6 +21,8 @@ def findrecording(vslist):
 	return recording
 
 # takes a string of integers separated by commas and returns a list
+
+
 def parseindicies(string):
 	if ',' in string:
 		splstring = string.split(',')
@@ -26,39 +32,53 @@ def parseindicies(string):
 		return [int(string)]
 
 # handlers for the currently connected cameras
-def handlecamops(vslist, request,activestreams, camerasconnected):
-      for i in range(0, camerasconnected):
-       if 'stopvs{}'.format(i+1) in request.form:
-        vslist[i].stop()
-       if 'stoprecordvs{}'.format(i+1) in request.form:
-        vslist[i].recordstop()
-       if 'startrecordvs{}'.format(i+1) in request.form:
-        vslist[i].recordstart()
-       if 'relaunchvs{}'.format(i+1) in request.form:
-        vslist[i].relaunch()
-       if 'startvs{}'.format(i+1) in request.form and activestreams <= 3:
-        vslist[i].start()
+
+
+def handlecamops(vslist, request, activestreams, recordingstreams, camerasconnected):
+	for i in range(0, camerasconnected):
+		if 'stopvs{}'.format(i+1) in request.form:
+			vslist[i].stop()
+		if 'recordvs{}'.format(i+1) in request.form:
+			if recordingstreams[i]:
+				vslist[i].recordstop()
+			else: 
+				vslist[i].recordstart()
+		if 'startrecordvs{}'.format(i+1) in request.form:
+			vslist[i].recordstart()
+		if 'relaunchvs{}'.format(i+1) in request.form:
+			vslist[i].relaunch()
+		if 'startvs{}'.format(i+1) in request.form and activestreams <= 3:
+			vslist[i].start()
+		if 'movetoleft{}'.format(i+1) in request.form:
+			temp = vslist.pop(i)
+			vslist.insert(0, temp)
 
 # grab the current timestamp and draw it on the frame
+
+
 def timestampframe(frame):
-    timestamp = datetime.datetime.now()
-    copiedframe = frame.copy()
-    cv2.putText(copiedframe, timestamp.strftime(
-            "%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-    return copiedframe
+	timestamp = datetime.datetime.now()
+	copiedframe = frame.copy()
+	cv2.putText(copiedframe, timestamp.strftime(
+		"%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+	return copiedframe
 
 # essentially all this does is calculate the amount of time to wait
 # before grabbing the next frame
+
+
 def calcwait(difference, targettime, lastwait):
-    timetosleep = lastwait - (difference-targettime)
-    if timetosleep < 0:
-        timetosleep = 0
-    if timetosleep > targettime:
-        timetosleep = targettime
-    return timetosleep
+	timetosleep = lastwait - (difference-targettime)
+	if timetosleep < 0:
+		timetosleep = 0
+	if timetosleep > targettime:
+		timetosleep = targettime
+	return timetosleep
 
 # turn a resolution string into valid integers
+
+
 def parseresolution(resstring):
 	if resstring == 'max':
 		return (10000, 10000)
@@ -68,4 +88,4 @@ def parseresolution(resstring):
 		height = int(res[1])
 		return (width, height)
 	else:
-		return (640,480)
+		return (640, 480)
